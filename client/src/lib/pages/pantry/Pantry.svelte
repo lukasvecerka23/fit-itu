1feb007aa58de573c6647d3f235d5a63d6ab14fc
Veronika Nevařilová <xnevar00@stud.fit.vutbr.cz>
2023-11-04 20:10:02 +0100

    Added layout of ingredients

--- client/src/lib/pages/pantry/Pantry.svelte
+++ client/src/lib/pages/pantry/Pantry.svelte
@@ -41,7 +41,7 @@
 </script>
 <div class="flex w-full">
     <Sidebar />
-    <div class="h-screen w-full bg-primary-white flex justify-center">
+    <div class="h-full w-full min-h-screen bg-primary-white flex justify-center">
         <div class="flex-col w-3/5 mx-auto">
                 <h1 class="text-black text-center text-4xl font-poppins py-4 font-semibold mb-0 mx-auto">Pantry</h1>
                 <div class="flex gap-2 items-center">
@@ -58,35 +58,12 @@
                         <img src={edit_icon} alt="Popis obrázku">
                     </button>
                 </div>
-                <div class="pt-10 flex-row justify-left">
+                <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 pt-10 justify-left">
                     {#each ingredients as ingredient}
                         <Ingredient ingredient={ingredient} />
                     {/each}
-                </div>
-                <div class="pt-10 flex-row justify-left">
-                    <p class="text-black mx-auto text-xl font-poppins mb-2">Vegetables</p>
-                    <hr class="w-full border border-black">
-                </div>
-                <div class="pt-10 flex-row justify-left">
-                    <p class="text-black mx-auto text-xl font-poppins mb-2">Dairy</p>
-                    <hr class="w-full border border-black">
-                </div>
-                <div class="pt-10 flex-row justify-left">
-                    <p class="text-black mx-auto text-xl font-poppins mb-2">Meats</p>
-                    <hr class="w-full border border-black">
-                </div>
-                <div class="pt-10 flex-row justify-left">
-                    <p class="text-black mx-auto text-xl font-poppins mb-2">Drinks</p>
-                    <hr class="w-full border border-black">
-                </div>
-                <div class="pt-10 flex-row justify-left">
-                    <p class="text-black mx-auto text-xl font-poppins mb-2">Sauces</p>
-                    <hr class="w-full border border-black">
-                </div>
-                <div class="pt-10 flex-row justify-left">
-                    <p class="text-black mx-auto text-xl font-poppins mb-2">Others</p>
-                    <hr class="w-full border border-black">
-                </div>
+                    <!-- A další komponenty -->
+                  </div>
         </div>
     </div>
 </div>
