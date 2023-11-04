d824296d9924b810e5fde1f67351415b3ed7c7b2
JachymDolezal <jachym.dolezal@gmail.com>
2023-11-04 19:05:49 +0100

    fix navbar, add recipes

---
+++ client/src/lib/components/Recipe.svelte
@@ -0,0 +1,21 @@
+<script>
+    // export let imageurl = '';
+    export let name = '';
+    export let description = '';
+  </script>
+  
+  <div class="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+    <!-- <img class="w-full" src={imageurl} alt="Food Image"> -->
+    <div class="px-6 py-4">
+      <div class="font-bold text-xl mb-2">
+        <h1> {name}</h1>
+      </div>
+      <p class="text-gray-700 text-base">
+        {description}
+      </p>
+    </div>
+  </div>
+  
+  <style>
+    /* Optionally, you can add Svelte-specific styles here */
+  </style>
--- client/src/lib/pages/recipes/Recipes.svelte
+++ client/src/lib/pages/recipes/Recipes.svelte
@@ -1,17 +1,26 @@
 <script>
     import Sidebar from '../../components/SideBar.svelte';
+    import Recipe from '../../components/Recipe.svelte';
     // You can add more script code here if needed
+    // Fetch pantry sections and set the first one as selected
+    let recipes = [];
+    fetch('https://fit-itu.hop.sh/api/collections/recipes/records')
+        .then(res => res.json())
+        .then(data => {
+            recipes = data.items;
+            console.log(recipes);
+        })
+        .catch(err => {
+            console.error(err);
+        });
   </script>
-  
+<div class="flex w-full">
   <Sidebar />
-  <div class="recipes-container">
-    <!-- Your recipes content goes here -->
+  <div class="h-screen w-full bg-primary-white flex justify-center">
+    {#each recipes as recipe}
+    <h1>
+    <Recipe name={recipe.name} description={recipe.description} />
+    </h1> 
+    {/each}
   </div>
-  
-  <style>
-    .recipes-container {
-      margin-left: 200px; /* Same as the width of the Sidebar */
-      padding: 1rem;
-      /* Rest of your styling for the recipes content */
-    }
-  </style>
+</div>
