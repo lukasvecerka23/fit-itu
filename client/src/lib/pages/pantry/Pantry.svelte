5d59f025342a04d2a508fad8a00beaa77a999ff3
Lukas Vecerka <vecerkalukas18@gmail.com>
2023-11-04 17:24:44 +0100

    implemented getting pantry sections from pocketbase

--- client/src/lib/pages/pantry/Pantry.svelte
+++ client/src/lib/pages/pantry/Pantry.svelte
@@ -1,18 +1,43 @@
 <script>
-    import { Link } from 'svelte-routing';
     import edit_icon from '../../../assets/edit_icon.svg';
+
+    let pantrySections = [];
+    let selectedSectionId = null;
+
+    // Fetch pantry sections and set the first one as selected
+    fetch('https://fit-itu.hop.sh/api/collections/pantrySections/records')
+        .then(res => res.json())
+        .then(data => {
+            pantrySections = data.items;
+            // Set the first section as selected, if there are any sections
+            if (pantrySections.length > 0) {
+                selectedSectionId = pantrySections[0].id;
+            }
+        })
+        .catch(err => {
+            console.error(err);
+        });
+
+    // Function to select a section
+    function selectSection(sectionId) {
+        selectedSectionId = sectionId;
+    }
+    
 </script>
 
 <div class="h-screen w-full bg-primary-white flex justify-center">
     <div class="flex-col w-1/2 mx-auto">
             <h1 class="text-black text-center text-4xl font-poppins py-4 font-semibold mb-0 mx-auto">Pantry</h1>
             <div class="flex gap-2 items-center">
-                <button class="bg-primary-green hover:bg-secondary-green text-white text-xs font-semibold rounded-2xl text-center w-20 h-5">
-                    <Link to="/home">Fridge</Link>
-                </button>
-                <button class="bg-primary-white hover:bg-white text-black text-xs font-semibold rounded-2xl text-center w-20 h-5 border-2 border-black">
-                    <Link to="/home">Freezer</Link>
-                </button>
+                {#each pantrySections as section}
+                    <button
+                        on:click={() => selectSection(section.id)}
+                        class={`${selectedSectionId === section.id ? 'bg-primary-green text-white' : 'bg-primary-white text-black'} hover:bg-secondary-green text-xs font-semibold rounded-2xl text-center w-20 h-5 border-2 border-black`}
+                    >
+                        {section.name}
+                    </button>
+                {/each}
+
                 <button class="bg-transparent h-5 w-5">
                     <img src={edit_icon} alt="Popis obrázku">
                 </button>
