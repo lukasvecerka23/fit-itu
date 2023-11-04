d8298b64125e0eacfca512a951e9490e680881d1
Veronika Nevařilová <xnevar00@stud.fit.vutbr.cz>
2023-11-04 17:55:37 +0100

    Started implementing ingredient list

--- client/src/lib/pages/pantry/Pantry.svelte
+++ client/src/lib/pages/pantry/Pantry.svelte
@@ -1,8 +1,10 @@
 <script>
     import edit_icon from '../../../assets/edit_icon.svg';
+    import Ingredient from './components/Ingredient.svelte';
 
     let pantrySections = [];
     let selectedSectionId = null;
+    let ingredients = [];
 
     // Fetch pantry sections and set the first one as selected
     fetch('https://fit-itu.hop.sh/api/collections/pantrySections/records')
@@ -18,15 +20,27 @@
             console.error(err);
         });
 
+    function getIngredient(sectionId){
+        fetch(`https://fit-itu.hop.sh/api/collections/ingredientInPantry/records?filter=(pantrySection='${sectionId}')&expand=ingredient`)
+        .then(res => res.json())
+        .then(data => {
+            ingredients = data.items;
+        })
+        .catch(err => {
+            console.error(err);
+        });
+    }
+
     // Function to select a section
     function selectSection(sectionId) {
         selectedSectionId = sectionId;
+        getIngredient(sectionId);
     }
     
 </script>
 
 <div class="h-screen w-full bg-primary-white flex justify-center">
-    <div class="flex-col w-1/2 mx-auto">
+    <div class="flex-col w-3/5 mx-auto">
             <h1 class="text-black text-center text-4xl font-poppins py-4 font-semibold mb-0 mx-auto">Pantry</h1>
             <div class="flex gap-2 items-center">
                 {#each pantrySections as section}
@@ -41,6 +55,11 @@
                 <button class="bg-transparent h-5 w-5">
                     <img src={edit_icon} alt="Popis obrázku">
                 </button>
+            </div>
+            <div class="pt-10 flex-row justify-left">
+                {#each ingredients as ingredient}
+                    <Ingredient ingredient={ingredient} />
+                {/each}
             </div>
             <div class="pt-10 flex-row justify-left">
                 <p class="text-black mx-auto text-xl font-poppins mb-2">Vegetables</p>
---
+++ client/src/lib/pages/pantry/components/Ingredient.svelte
@@ -0,0 +1,7 @@
+<script>
+    export let ingredient = {};
+</script>
+
+<div>
+    <p>{ingredient.expand.ingredient.name}</p>
+</div>
