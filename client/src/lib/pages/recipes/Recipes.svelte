d1d5b4f122f44e8903c5388ef2e101338b3ef714
JachymDolezal <jachym.dolezal@gmail.com>
2023-12-16 17:08:00 +0100

    add updated recipe list and detail

--- client/src/lib/pages/recipes/Recipes.svelte
+++ client/src/lib/pages/recipes/Recipes.svelte
@@ -1,9 +1,15 @@
 <script>
     import Sidebar from '../../components/SideBar.svelte';
     import Recipe from '../../components/Recipe.svelte';
+    import SlideUpOverlay from '../../components/SlideUpOverlay.svelte';
     // You can add more script code here if needed
     // Fetch pantry sections and set the first one as selected
     let recipes = [];
+    let showModal = false;
+    let selectedRecipe = null;
+    let selectedRecipeData = null;
+    let ingredients = [];
+    let loading = false;
     fetch('https://fit-itu.hop.sh/api/collections/recipes/records')
         .then(res => res.json())
         .then(data => {
@@ -13,14 +19,101 @@
         .catch(err => {
             console.error(err);
         });
+
+    async function openModal(recipe) {
+      const response = await fetch(`https://fit-itu.hop.sh/api/collections/recipes/records/${recipe.id}?expand=steps,diet,ingredients,ingredients.ingredientId,ingredients.ingredientId.unit`);
+      if (!response.ok) {
+        // If the response is not okay, throw an error
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      selectedRecipeData = await response.json();
+      // loading = true;
+      console.log(selectedRecipeData);
+      loading = true;
+      selectedRecipe = recipe;
+      showModal = true;
+      loading = false;
+    }
+
+    function closeModal() {
+      showModal = false;
+    }
   </script>
+
 <div class="flex w-full">
   <Sidebar />
-  <div class="h-screen w-full bg-primary-white flex justify-center">
-    {#each recipes as recipe}
-    <h1>
-    <Recipe name={recipe.name} description={recipe.description} />
-    </h1> 
-    {/each}
+  <div class="h-screen w-full bg-primary-white overflow-auto">
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-6 p-2">
+      {#each recipes as recipe}
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        <!-- svelte-ignore a11y-no-static-element-interactions -->
+        <div class="rounded-lg cursor-pointer" on:click={() => openModal(recipe)}>
+          <Recipe name={recipe.name} description={recipe.description} imageurl={recipe.image}/>
+        </div>
+      {/each}
+    </div>
   </div>
 </div>
+
+
+<SlideUpOverlay bind:show={showModal}>
+
+  <div class={`fixed inset-0 bg-black bg-opacity-50 z-50 ${showModal ? 'flex' : 'hidden'} items-end`}>
+    {#if loading}
+    <div class="w-full max-w-screen-md mx-auto bg-white rounded-t-lg overflow-hidden">
+    <div class="px-4 pt-4">
+      <h2 class="text-2xl font-bold">Loading ingredients...</h2>
+    </div>
+    </div>
+    {:else}
+    <div class="w-full max-w-screen-md mx-auto bg-white rounded-t-lg overflow-hidden">
+
+      <!-- Recipe Title -->
+      <div class="px-4 pt-4">
+        <h2 class="text-2xl font-bold">{selectedRecipe.name}</h2>
+      </div>
+
+      <!-- Recipe Image -->
+      <img src={selectedRecipe.image} alt={selectedRecipe.name} class="w-full h-64 object-cover" />
+
+      <!-- Ingredients List -->
+      <div class="px-4">
+        <div class="grid grid-cols-3 gap-4 items-center">
+          <div class="font-bold">Ingredients</div>
+          <div class="col-span-2 font-bold text-right">Amount</div>
+          {#each selectedRecipeData.expand.ingredients as ingredient, index}
+            <div>
+              <li class="list-disc list-inside">{ingredient.expand.ingredientId.name}</li>
+            </div>
+            <div class="col-span-2 text-right">
+              {ingredient.amount} {ingredient.expand.ingredientId.expand.unit.name}
+              <!-- Add icons here as needed -->
+            </div>
+          {/each}
+        </div>
+      </div>
+
+      <!-- Steps -->
+      <div class="px-4 my-4">
+        <h3 class="font-semibold text-lg mb-2">Steps</h3>
+        <ol class="list-decimal pl-5">
+          {#each selectedRecipeData.expand.steps as step, index}
+            {step.text}<br>
+          {/each}
+        </ol>
+      </div>
+  
+      <!-- Action Buttons -->
+      <div class="flex justify-between items-center px-4 py-4">
+        <button class="bg-green-500 text-white px-6 py-2 rounded-full font-bold">Cook</button>
+        <button on:click={closeModal} class="bg-red-500 text-white px-6 py-2 rounded-full font-bold">Close</button>
+        <!-- <button class="text-red-500">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <!-- Heart icon SVG path -->
+          <!-- </svg> -->
+        <!-- </button> -->
+      </div>
+    </div>
+    {/if}
+  </div>
+</SlideUpOverlay>
