8ad4e532dc601004b3dcf82229f85fabb2fbaf5c
JachymDolezal <jachym.dolezal@gmail.com>
2023-12-16 20:13:43 +0100

    add functional recipe detail

--- client/src/lib/pages/recipes/Recipes.svelte
+++ client/src/lib/pages/recipes/Recipes.svelte
@@ -6,11 +6,14 @@
     import circle_checked from '../../../assets/circle_checked.svg';
     import close_icon_black from '../../../assets/close_icon_black.svg';
     import basket_to_buy from '../../../assets/basket_to_buy.svg';
+    import basket_in_shopping_list from '../../../assets/basket_in_shopping_list.svg';
     // You can add more script code here if needed
     // Fetch pantry sections and set the first one as selected
     let recipes = [];
     let showModal = false;
     let selectedRecipe = null;
+    let missingIngredients = [];
+    let missingIngredientsShoppingList = [];
     export let selectedRecipeData = null;
     let ingredients = [];
     let loading = false;
@@ -24,6 +27,9 @@
             console.error(err);
         });
 
+
+
+
     async function openModal(recipe) {
       loading = true;
       const response = await fetch(`https://fit-itu.hop.sh/api/collections/recipes/records/${recipe.id}?expand=steps,diet,ingredients,ingredients.ingredientId,ingredients.ingredientId.unit`);
@@ -31,9 +37,45 @@
         // If the response is not okay, throw an error
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
+
       selectedRecipeData = await response.json();
       // loading = true;
       console.log(selectedRecipeData);
+
+      // Fetch the ingredients in the pantry
+      const pantryResponse = await fetch('https://fit-itu.hop.sh/api/collections/ingredientInPantry/records?expand=ingredient');
+      if (!pantryResponse.ok) throw new Error(`HTTP error! Status: ${pantryResponse.status}`);
+      const pantryData = await pantryResponse.json();
+
+      // Create a map of pantry ingredients for easy lookup
+      const pantryIngredientsMap = new Map(pantryData.items.map(item => [item.expand.ingredient.name, item]));
+
+      console.log("pantry",pantryIngredientsMap);
+
+      // Determine missing ingredients
+      missingIngredients = selectedRecipeData.expand.ingredients.filter(ingredient => {
+        const pantryItem = pantryIngredientsMap.get(ingredient.expand.ingredientId.name);
+        return !pantryItem || pantryItem.amount < ingredient.amount;
+      });
+
+      console.log("missing ingredients",missingIngredients);
+
+
+      // Fetch the ingredients in the shopping list
+      const shoppingListResponse = await fetch('https://fit-itu.hop.sh/api/collections/ingredientInShoppingList/records?expand=ingredient');
+      if (!shoppingListResponse.ok) throw new Error(`HTTP error! Status: ${shoppingListResponse.status}`);
+      const shoppingListData = await shoppingListResponse.json();
+
+      // Create a map of shopping list ingredients for easy lookup
+      const shoppingListIngredientsMap = new Map(shoppingListData.items.map(item => [item.expand.ingredient.name, item]));
+
+      console.log("shopping list",shoppingListIngredientsMap);
+
+      // Determine missing ingredients
+      missingIngredientsShoppingList = selectedRecipeData.expand.ingredients.filter(ingredient => {
+        const shoppingListItem = shoppingListIngredientsMap.get(ingredient.expand.ingredientId.name);
+        return !shoppingListItem || shoppingListItem.amount < ingredient.amount;
+      });
 
       if (selectedRecipeData && selectedRecipeData.expand && selectedRecipeData.expand.steps) {
         selectedRecipeData.expand.steps.sort((a, b) => a.stepNumber - b.stepNumber);
@@ -49,9 +91,63 @@
       showModal = false;
     }
 
-    function testbutton(){
-      console.log("test");
+    async function addIngredientToShoppingList(ingredient){
+      const shoppingListUrl = 'https://fit-itu.hop.sh/api/collections/ingredientInShoppingList/records';
+    const ingredientId = ingredient.expand.ingredientId.id; // The ID of the ingredient
+    const amountToAdd = ingredient.amount;
+    try {
+        // Check if ingredient is in the shopping list
+        const shoppingListResponse = await fetch(shoppingListUrl);
+        if (!shoppingListResponse.ok) {
+            throw new Error(`HTTP error! Status: ${shoppingListResponse.status}`);
+        }
+        const shoppingList = await shoppingListResponse.json();
+
+        // Find if the ingredient already exists in the shopping list
+        const existingItem = shoppingList.items.find(item => item.ingredient === ingredientId);
+
+        if (existingItem) {
+            // Ingredient exists, update the amount
+            const updateUrl = `${shoppingListUrl}/${existingItem.id}`; // Assuming the item's ID is in `existingItem.id`
+            const updatedAmount = (amountToAdd - existingItem.amount) + existingItem.amount; // Add only the difference to the existing amount
+
+            const patchResponse = await fetch(updateUrl, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ amount: updatedAmount }),
+            });
+
+            if (!patchResponse.ok) {
+                throw new Error(`HTTP error! Status: ${patchResponse.status}`);
+            }
+
+            console.log("Ingredient amount updated in shopping list");
+        } else {
+            // Ingredient does not exist, add new item
+            const postResponse = await fetch(shoppingListUrl, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    userId: "uvh48ynbmnnmydx", // Constant user ID sine we don't have authentication
+                    ingredient: ingredientId,
+                    amount: amountToAdd,
+                }),
+            });
+
+            if (!postResponse.ok) {
+                throw new Error(`HTTP error! Status: ${postResponse.status}`);
+            }
+
+            console.log("Ingredient added to shopping list");
+        }
+    } catch (error) {
+        console.error("Error processing shopping list:", error);
     }
+}
 
 
   </script>
@@ -105,15 +201,19 @@
               <div class="flex items-center space-x-4">
               {ingredient.amount} {ingredient.expand.ingredientId.expand.unit.name}
               <!-- Add icons here as needed -->
-              {#if 0}
-              <img src={circle_checked} alt="arrow" class="w-6 h-6" />
-              {:else}
+              {#if missingIngredients.some(missingIngredient => missingIngredient.expand.ingredientId.name === ingredient.expand.ingredientId.name)}
               <img src={close_icon_black} alt="arrow" class="w-6 h-6" />
               <!-- svelte-ignore a11y-click-events-have-key-events -->
               <!-- svelte-ignore a11y-no-static-element-interactions -->
-              <button class="w-6 h-6" on:click={() => testbutton()}>
-              <img src={basket_to_buy} alt="arrow"/>
+              {#if missingIngredientsShoppingList.some(missingIngredient => missingIngredient.expand.ingredientId.name === ingredient.expand.ingredientId.name)}
+              <button class="w-6 h-6" on:click={() => {addIngredientToShoppingList(ingredient);}}>
+              <img src={basket_to_buy} alt="ingredient not in shopping list"/>
               </button>
+              {:else}
+              <img src={basket_in_shopping_list} alt="ingredient in shopping list"  class="w-6 h-6"/>
+              {/if}
+              {:else}
+              <img src={circle_checked} alt="arrow" class="w-6 h-6" />
               {/if}
                 <!-- Icon SVG -->
               </div>
